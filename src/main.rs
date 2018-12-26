4c853e360276a310a8882f9da9182c923fb4cbd7
Erik Bremen <strtok@strtok.net>
2018-12-26 15:41:00 -0800

    Add readline loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
+use rustyline::error::ReadlineError;
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+    debug!("starting");
+
+    let mut rl = rustyline::Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline("lisp> ");
+        match readline {
+            Ok(line) => println!("line: {:?}", line),
+            Err(ReadlineError::Interrupted) => {
+                break;
+            },
+            Err(ReadlineError::Eof) => {
+                break;
+            },
+            Err(err) => {
+                error!("error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
