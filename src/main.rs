10142e478ebd98a88c99ffa772f393e558f3674c
Erik Bremen <strtok@strtok.net>
2019-01-15 08:26:49 -0800

    Hook parser up to repl

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,32 @@ extern crate regex;
 #[macro_use] mod parser;
 
 use rustyline::error::ReadlineError;
+use crate::parser::*;
 
 fn main() {
     pretty_env_logger::init();
     debug!("starting");
 
-    let mut rl = rustyline::Editor::<()>::new();
+    let whitespace = || {
+        repeat(regex("[\\s]"))
+    };
+    let parser = seq!(ch('('), whitespace(), ch(')'));
 
+    let mut rl = rustyline::Editor::<()>::new();
     loop {
         let readline = rl.readline("lisp> ");
         match readline {
-            Ok(line) => println!("line: {:?}", line),
+            Ok(line) => {
+                let result = parser(&line);
+                match result {
+                    ParseResult::Value {value: _, remaining_input: _} => {
+                        info!("success!");
+                    }
+                    ParseResult::Error{text} => {
+                        info!("error: {}", text);
+                    }
+                }
+            },
             Err(ReadlineError::Interrupted) => {
                 break;
             },
